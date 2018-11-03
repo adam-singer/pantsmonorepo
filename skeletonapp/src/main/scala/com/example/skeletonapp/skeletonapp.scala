79a0db1726b46cd1ca439e4c4ef53e8794ec0f80
Matthew Dickinson <matt@mattd.org>
2018-11-03 09:17:03 -0600

    comment

--- skeletonapp/src/main/scala/com/example/skeletonapp/skeletonapp.scala
+++ skeletonapp/src/main/scala/com/example/skeletonapp/skeletonapp.scala
@@ -7,6 +7,7 @@ object SkeletonApp extends SkeletonAppServer
 
 class SkeletonAppServer extends TwitterServer {
 
+  // this is a comment
   override val name: String = "SkeletonApp"
 
   override protected def modules: Seq[Module] = Seq(new DefaultServerModule)
